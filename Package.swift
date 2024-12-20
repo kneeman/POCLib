8cc6b24a4f110c59e7e714fc4532c667f60aacc8
Ben Conway <ben.conway@mindbodyonline.com>
2024-12-20 14:59:30 -0500

    Updating iOS.

--- Package.swift
+++ Package.swift
@@ -1,28 +1,9 @@
 // swift-tools-version:5.7
 import PackageDescription
 
-// let package = Package(
-//     name: "POCLib",
-//     platforms: [
-//         .iOS(.v13)
-//     ],
-//     products: [
-//         .library(
-//             name: "POCLib",
-//             targets: ["POCLib"]
-//         ),
-//     ],
-//     targets: [
-//         .binaryTarget(
-//             name: "poc_lib",
-//             path: "poc-lib/build/xcframework/poc_lib.xcframework"
-//         )
-//     ]
-// )
-
 let package = Package(
     name: "POCLib",
-    platforms: [.macOS(.v13), .iOS(.v13)],
+    platforms: [.macOS(.v13)],
     dependencies: [
         .package(url: "https://github.com/kneeman/POCLib.git", branch: "main")
     ],
