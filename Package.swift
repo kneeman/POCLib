ff0d5fe660192f34aba4cdfd4c7b7315c8b3637a
Ben Conway <ben.conway@mindbodyonline.com>
2024-12-20 14:16:05 -0500

    Changing path.

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     name: "POCLib",
     platforms: [.macOS(.v13)],
     dependencies: [
-        .package(url: "https://github.com/kneeman/POCLib.git", branch: "main")
+        .package(url: "https://github.com/kneeman/POCLib.git", branch: "feautre/iOS-integration-2")
     ],
     targets: [
         .binaryTarget(
