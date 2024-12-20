5b5e90dcf6f277fbc53946e2dfdc63d38767b8e1
Ben Conway <ben.conway@mindbodyonline.com>
2024-12-20 14:24:53 -0500

    Updatings platform.

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ import PackageDescription
 
 let package = Package(
     name: "POCLib",
-    platforms: [.macOS(.v13)],
+    platforms: [.iOS(.v13)],
     dependencies: [
         .package(url: "https://github.com/kneeman/POCLib.git", branch: "main")
     ],
